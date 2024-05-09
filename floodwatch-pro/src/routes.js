c5e31cc6255e6ecfc624a6dfe211a94a4ca75d53
Stephen Ochieng <materiamystic@gmail.com>
2024-05-09 03:05:41 +0300

    Adds routing

---
+++ floodwatch-pro/src/routes.js
@@ -0,0 +1,23 @@
+import Home from "./Home";
+
+const routes = [
+  {
+    path: "/",
+    element: <Home />,
+  },
+  //   {
+  //     path: "/about",
+  //     element: <JafarAbout />,
+  //   },
+  //   {
+  //     path: "/tips",
+
+  //     element: <IanTips />,
+  //   },
+  //   {
+  //     path: "/:id",
+  //     element: <MichelleDetails />,
+  //   },
+];
+
+export default routes;
