6f2990b70e10244fe3b59f63a5d36a5b1417f184
emmanuel <emmanuel.kisoso@student.moringaschool.com>
2024-05-09 06:52:03 +0300

    Added navbar

--- floodwatch-pro/src/App.js
+++ floodwatch-pro/src/App.js
@@ -1,13 +1,17 @@
 import './App.css';
 import Footer from './components/Footer';
+// import { Routes,Route } from 'react-router-dom';
+import NavBar from './components/NavBar';
 
 function App() {
   return (
     <div>
-      <div
-      style={{
-                    minHeight: "400px",
-                }}>
+      <div style={{minHeight: "400px",}}>
+      <NavBar />
+        {/* <Routes>
+          <Route path='/' element={ <Home /> } />
+          <Route path='/' element={ <Tips /> } />
+        </Routes> */}
       </div>
       <Footer />
     </div>
