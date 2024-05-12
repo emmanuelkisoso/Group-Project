bdf43fda8d642d5417990bed1143bb6c435c1b68
emmanuelkisoso <162184054+emmanuelkisoso@users.noreply.github.com>
2024-05-13 01:32:10 +0300

    Update Home.js

--- floodwatch-pro/src/Home.js
+++ floodwatch-pro/src/Home.js
@@ -1,13 +1,18 @@
+
 import { useState, useEffect } from "react";
 import { Link } from "react-router-dom";
-import NavBar from "./components/NavBar";
-import Footer from "./components/Footer";
-import FloodDetails from "./components/FloodDetails";
+import "./Home.css";
+import floodGif from "./images/flooded-house.png";
+import Header from "./Header";
+import Footer from "./Footer";
 
+//Homepage component
 function Home() {
   const [stats, setStats] = useState([]);
-  let [initNumber, setInitNumber] = useState(10);
+  // let [initNumber, setInitNumber] = useState(10);
+  const [activeItem, setActiveItem] = useState("home");
 
+  //fetch data from db.json
   useEffect(() => {
     fetch("http://localhost:3000/floodstats")
       .then((r) => r.json())
@@ -15,52 +20,64 @@ function Home() {
       .catch((error) => alert(error.message));
   }, []);
 
-  const simulateCasualtyIncrease = () => {
-    setInterval(() => {
-      setInitNumber(initNumber + 2);
-    }, 10000);
-  };
+  //text animation set up
+  const observer = new IntersectionObserver((entries) => {
+    entries.forEach((entry) => {
+      if (entry.isIntersecting) {
+        entry.target.classList.add("show");
+      } else {
+        entry.target.classList.remove("show");
+      }
+    });
+  });
 
-  simulateCasualtyIncrease();
+  const hiddenItems = document.querySelectorAll(".hidden");
+  hiddenItems.forEach((el) => observer.observe(el));
+
+  //handle routing to another page using Link
+  // function handleClick(id) {
+  //   <Link to={`/${id}`}></Link>;
+  // }
 
   return (
     <>
-      {/* <NavBar /> */}
-      <div className="container">
-        <div className="row">
-          <div className="col-6 border w-25">
-            <div className="col-10 border border-danger mx-auto">
-              <h5 className="">Casualty count</h5>
-              Number of displaced people: {initNumber}
-            </div>
-          </div>
-          <div className="col-6">
-            <div className="row border border-danger">
-              {stats.map((stat) => (
-                <div className="card col-3 m-3" key={stat.id}>
-                  <Link to={`/floodDetails/${stat.id}`}><h5 className="card-title badge text-bg-dark m-2">{stat.continent}</h5></Link>
-               
-                  <div className="card-body">
-                    <h5>{stat.location}</h5>
-                    {stat.alertLevel === "red" ? (
-                      <span className="badge rounded-pill text-bg-danger float-end mb-0">
-                        severe
-                      </span>
-                    ) : (
-                      <span className="badge rounded-pill text-bg-warning float-end mb-0">
-                        moderate
-                      </span>
-                    )}
-                  </div>
+      <Header item="home" />
+
+      <div className="floodItemsBox">
+        {stats.map((stat) => {
+          return (
+            <div
+              key={stat.id}
+              className="flexItems"
+              // style={{ border: "5px solid white" }}
+            >
+              <div style={{ width: "100%" }}>
+                <Link to={`/${stat.id}}`}>
+                  <img
+                    src={stat.pictureUrl}
+                    alt="pic"
+                    // onClick={() => handleClick(stat.id)}
+                  />
+                </Link>
+              </div>
+
+              <div className="content hidden">
+                <h3>{stat.location}</h3>
+
+                <div className="alert hidden">
+                  <img
+                    src={stat.alertLevel === "red" ? floodGif : ""}
+                    alt="pic"
+                    style={{ width: "30px" }}
+                  />
                 </div>
-              ))}
+              </div>
             </div>
-          </div>
-        </div>
+          );
+        })}
       </div>
-      {/* <Footer /> */}
+      <Footer />
     </>
   );
 }
-
 export default Home;
