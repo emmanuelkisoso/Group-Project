3c109e3c77693fa84f52c482f3e8edea96263ee3
emmanuelkisoso <162184054+emmanuelkisoso@users.noreply.github.com>
2024-05-13 00:44:50 +0300

    Update App.js

--- floodwatch-pro/src/App.js
+++ floodwatch-pro/src/App.js
@@ -1,12 +1,7 @@
-import React from 'react'
-import Tips from './components/TipsPage';
+import "./App.css";
 
 function App() {
-  return (
-    <div className="App">
-      <Tips />
-    </div>
-  );
+  return <div></div>;
 }
 
 export default App;
